25dee71ce2d4d7d1df23aa73d43d477a1fcad5ed
rdzfv <xuyuyan999@126.com>
2022-03-02 17:46:33 +0800

    fix：改回了主界面的验证逻辑

--- pages/main/main.js
+++ pages/main/main.js
@@ -71,20 +71,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
     // 登录
     wx.login({
       success: function (res) {
@@ -137,6 +123,20 @@ Page({
         }
       }
     })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
   },
 
   /**
